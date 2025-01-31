d7399aa26f8fd9c3f6ee6299558481bb6a327d85
ydroulis <lombardi.droulis@gmail.com>
2025-01-31 19:32:57 -0300

    initial commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,6 +2,7 @@ module.exports = {
   // Lint & Prettify TS and JS Files
   '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix . ${filenames.join(' --file')}``npm test -- --findRelatedTests ${filenames.join(' ')}`
+    `npm run lint --fix . ${filenames.join(' --file')}`,
+    `npm test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 }
